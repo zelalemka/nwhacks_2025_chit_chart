d6ff4039edfc4034225c0597c4c5f9a112b0b107
Stephanie Feng <stephanie.c.feng@gmail.com>
2025-01-19 08:11:26 -0800

    add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from './components/Layout/Home';
 import { MedicalInterface } from './components/Layout/MedicalInterface';
 
 function App() {
   return (
-    <div className="App">
-      <MedicalInterface />
-    </div>
-  );
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/doctor" element={<MedicalInterface />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
