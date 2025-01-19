b8b6ed5a8d5d63e7300e249ef8d37e60f8e59547
charity-g <charity.grey1@gmail.com>
2025-01-19 01:01:44 -0800

    router react boilerplate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { MedicalInterface } from './components/Layout/MedicalInterface';
+import {  BrowserRouter,  Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <MedicalInterface />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      <Routes>
+        <Route path="/" element=<MedicalInterface /> />
+        {/* <Route path="/relativepath" element=<element /> /> */}
+      </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
