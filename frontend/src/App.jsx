6f5f9ca20a3798e1335e6a153c17b1e1ace93d00
charity-g <charity.grey1@gmail.com>
2025-01-18 22:12:54 -0800

    transform into jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { MedicalInterface } from './components/Layout/MedicalInterface';
 
 function App() {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'regenerator-runtime/runtime';
 import './index.css'
-import App from './App.tsx'
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
